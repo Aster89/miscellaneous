c46e483ea6637109d91feda9831ecfb0a1544d9e
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2020-04-09 17:58:17 +0100

    My bash blockify

---
+++ blockSpotifyAds.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env -S bash
+
+### exit if playerctl and pactl are not available ###
+which playerctl 2> /dev/null 1>&2 && which pactl 2> /dev/null 1>&2 || exit
+
+### functions ###
+# check if Spotify is playing ads
+shouldMute() {
+  [[ $(playerctl --player=spotify metadata 2> /dev/null \
+    | sed 's/^.*:title *//p;d') == 'Advertisement' ]]
+}
+
+# retrieve the sink Spotify is using
+getCurrentSink() {
+  pactl list \
+    | sed -E '/^Sink Input #/,/^$/{/^\s*Sink:\s*/{s///;h};/application\.name.*Spotify/{x;p}};d'
+}
+
+# mute/unmute
+setMute() {
+  # fallback on old sink if new one is empty
+  # (this happens when terminating Spotify)
+  sinkNew="$(getCurrentSink)"
+  [[ -n "$sinkNew" ]] && sink="$sinkNew"
+  pactl set-sink-mute "$sink" $1 2> /dev/null && pkill -RTMIN+1 i3blocks
+}
+
+### unmute upon termination ###
+trap 'setMute 0; exit' SIGINT SIGTERM EXIT
+
+### main loop ###
+while true; do
+  sleep 1 && shouldMute && setMute 1 || setMute 0
+done
