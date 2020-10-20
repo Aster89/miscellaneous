cd72227b1ffa944584d386c2356735a3d74cbdc4
Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
2020-10-20 08:24:58 +0100

    setscreens: shellcheck fix

--- setscreens.sh
+++ setscreens.sh
@@ -4,4 +4,4 @@ source "$(dirname "$0")/commons.sh" || exit 1
 
 [[ -x "${SCRIPT:=$(dirname "$0")/make_setscreens.sh}" ]] \
   || { error 'make_setscreens.sh script not found beside setscreens.sh.' && exit 1; }
-$($SCRIPT)
+eval "$("$SCRIPT")"
